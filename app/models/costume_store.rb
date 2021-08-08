9b5f29932a382b494fb5e61144d8c879d5762680
thakaj <taulant.hakaj@scranton.edu>
2021-08-08 12:32:24 -0400

    commit tree d25e2c066dadfa21c9c967a07d973b380abe8639

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,16 @@
 # Create your CostumeStore class here
+class CostumeStore < ActiveRecord::Base
+    def change
+        create_table :costumes do |t| (
+            t.string :name
+            t.float :price
+            t.string :size
+            t.string :image_url 
+            t.timestamps :create_at
+            t.timestamps :updated_at
+
+        )
+    end
+
+
+end
